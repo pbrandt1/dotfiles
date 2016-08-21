80ecf9b1842cdea985e8d4e128c1e179451602ff
Peter Brandt <peter.m.brandt@gmail.com>
2016-08-21 08:31:55 -0400

    vim stuff

---
+++ vim/install.sh
@@ -0,0 +1,5 @@
+cd ~/.vim/bundle
+git clone https://github.com/kien/ctrlp.vim
+git clone --depth=1 https://github.com/scrooloose/syntastic.git
+git clone git://github.com/tpope/vim-sensible.git
+git clone https://github.com/pangloss/vim-javascript.git
