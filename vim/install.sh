d743468c2e2987b49149d9b1c8986a5da8c23bea
pbrandt1 <peter.m.brandt@gmail.com>
2016-09-25 16:06:27 -0400

    vim updates

--- vim/install.sh
+++ vim/install.sh
@@ -6,3 +6,4 @@ git clone https://github.com/kien/ctrlp.vim
 git clone --depth=1 https://github.com/scrooloose/syntastic.git
 git clone git://github.com/tpope/vim-sensible.git
 git clone https://github.com/pangloss/vim-javascript.git
+git clone https://github.com/ervandew/supertab
