88bb11d3f35fd5e1984e660c2b5b5ff05c4fa6c6
pbrandt1 <peter.m.brandt@gmail.com>
2017-03-21 07:56:06 -0400

    updating dotfiles

--- vim/install.sh
+++ vim/install.sh
@@ -11,4 +11,4 @@ git clone git://github.com/tpope/vim-commentary.git
 git clone https://github.com/raichoo/purescript-vim
 git clone https://github.com/mxw/vim-jsx
 git clone https://github.com/vim-airline/vim-airline
-
+git clone https://github.com/thinca/vim-localrc
